fe9aeff8657f945db039d7b29d9fac38481a996c
Tilmann Diepenbruck <tdiepenb@smail.uni-koeln.de>
2024-12-19 13:14:12 +0100

    some teest, but still having shape missmatch errors

--- exercise-04/mnist01-base.py
+++ exercise-04/mnist01-base.py
@@ -1,25 +1,50 @@
 import csv
 from sklearn.naive_bayes import CategoricalNB
 from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 def read_dataset(filename):
     x = []
     y = []
-    with open(filename, newline='') as csvfile:
+    with open(filename, newline="") as csvfile:
         next(csvfile)
-        reader = csv.reader(csvfile, delimiter=',')
+        reader = csv.reader(csvfile, delimiter=",")
         for row in reader:
             y.append(int(row[0]))
             x.append([int(pixel) for pixel in row[1:]])
-    return (x, y)
+    return (np.array(x), np.array(y))
 
 
-x_train, y_train = read_dataset("mnist01/train.csv")
+x_train, y_train = read_dataset("./mnist01/train.csv")
 
-x_test, y_test = read_dataset("mnist01/test.csv")
+x_test, y_test = read_dataset("./mnist01/test.csv")
 
 print(f"Training items: {len(x_train)}")
 print(f"Test items: {len(x_test)}")
 
+print("train image", x_train[0])
+print("train_true", y_train[0])
+print("train image shape", len(x_train[0]))
+
+print("test image", x_test[0])
+print("test_true", y_test[0])
+print("test image shape", len(x_test[0]))
+
+print(f"x_train shape: {x_train.shape}")
+print(f"x_test shape: {x_test.shape}")
+
+
+bayesModel = CategoricalNB()
+bayesModel.fit(x_train, y_train)
+
+bayesPredict = bayesModel.predict(x_test)
+
+
+print("Accuracy = {} %".format(accuracy_score(y_test, bayesPredict) * 100))
+print(
+    "Classification Report \n {}".format(
+        classification_report(y_test, bayesPredict, labels=range(0, 10))
+    )
+)
